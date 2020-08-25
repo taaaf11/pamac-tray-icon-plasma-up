a99d09834554e0144128e3f54314e15fdcb869a1
Artem Grinev <agrinev98@gmail.com>
2020-08-25 22:42:53 +0400

    Added run guard to prevent tray from opening multiple instances

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <QApplication>
 #include <libnotify/notify.h>
 #include <pamac_tray.h>
+#include <run_guard.h>
 int main(int argc, char *argv[])
 {
+    RunGuard guard("org.manjaro.pamac.tray-plasma");
+    if(!guard.tryToRun()){
+        return 0;
+    }
+
     QApplication a(argc, argv);
     notify_init("Package Manager");
 
