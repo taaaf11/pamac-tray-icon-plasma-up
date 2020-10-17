8ecd2c841a3910b9f3cfdcf67c9bad699652c971
Artem Grinev <agrinev98@gmail.com>
2020-10-17 22:33:48 +0300

    Prepended "Pamac" to tray name in case of using Discover along with this indicator

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     QApplication::setQuitOnLastWindowClosed(false);
-    QApplication::setApplicationName("Updates");
+    QApplication::setApplicationName("Pamac Updates");
     PamacTray tray;
 
 
