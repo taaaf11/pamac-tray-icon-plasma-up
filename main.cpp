839e592fe4da38e4fafa3a8a1c2cba94324139c4
Artem Grinev <agrinev98@gmail.com>
2020-10-17 22:27:49 +0300

    Fixed tray name

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     QApplication::setQuitOnLastWindowClosed(false);
-    QApplication::setApplicationName("pamac-tray-plasma");
+    QApplication::setApplicationName("Updates");
     PamacTray tray;
 
 
